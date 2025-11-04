1aeeeb4249d652de4cb01cf34e1691c584c4c6e8
laleremitcode-rgb <laleremitcode@gmail.com>
2025-11-04 07:14:42 +0530

    Add files via upload

---
+++ Hackerrank ads-rem -ads/Q1.c
@@ -0,0 +1,19 @@
+ Q.Task is to reverse an array of integers.
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);  // number of elements
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);  // input array elements
+    }
+
+    // print reversed array
+    for (int i = n - 1; i >= 0; i--) {
+        printf("%d", arr[i]);
+        if (i > 0) printf(" "); // space between elements
+    }
+
+    return 0;
+}
